6b792fff2cd0dc846337d2815a0edf104a498537
Marina <116947921+Marscorpion@users.noreply.github.com>
2023-03-26 12:37:49 +0300

    Add files via upload

---
+++ appointment/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, reverse, redirect
+from django.views import View
+from django.core.mail import send_mail
+from datetime import datetime
+
+from .models import Appointment
+
+
+class AppointmentView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'make_appointment.html', {})
+
+    def post(self, request, *args, **kwargs):
+        appointment = Appointment(
+            date=datetime.strptime(request.POST['date'], '%Y-%m-%d'),
+            client_name=request.POST['client_name'],
+            message=request.POST['message'],
+        )
+        appointment.save()
+
+        # send_mail(
+        #     subject=f'{appointment.client_name} {appointment.date.strftime("%Y-%M-%d")}',
+        #     message=appointment.message,
+        #     from_email='seafoamskl@yandex.ru',
+        #     recipient_list=[]
+        # )
+
+        return redirect('appointments:make_appointment')
+
